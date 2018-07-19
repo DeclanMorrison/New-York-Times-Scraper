28c0a81113c416ac64f497560ab12186e7c639e8
Bobby H <rhoffman103@gmail.com>
2018-07-18 21:14:06 -0400

    js btn

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,25 +1,28 @@
+$(document).ready(function() {
 
+  $("#search-btn").on("click", function() {
 
-$("#search-btn").on("click", function() {
+  var searchTerm;
+  var apiKey = "74b0b534002f477ead1026e1b98f1377";
 
-var searchTerm;
-var apiKey = "74b0b534002f477ead1026e1b98f1377";
+  var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+  url += '?' + $.param({
+    'api-key': apiKey,
+    'q': searchTerm
+  });
+  $.ajax({
+    url: url,
+    method: 'GET',
+  }).done(function(result) {
+    console.log(result);
+  }).fail(function(err) {
+    throw err;
+  });
 
-var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
-url += '?' + $.param({
-  'api-key': apiKey,
-  'q': searchTerm
-});
-$.ajax({
-  url: url,
-  method: 'GET',
-}).done(function(result) {
-  console.log(result);
-}).fail(function(err) {
-  throw err;
-});
+    $(document).on("click", "#search-btn", function(event) {
+    searchTerm = $("#search-text").text();
+    console.log(url)
+    });
 
-
- searchTerm = $("#search-text").text();
-
+  });
 });
