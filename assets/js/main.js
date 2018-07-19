2466c76120a6b34a86126e8f3c64c54a3540b1cb
Peter Griffin <petergrieve33@gmail.com>
2018-07-18 20:24:26 -0400

    Base js code

---
+++ assets/js/main.js
@@ -0,0 +1,20 @@
+
+
+var searchTerm;
+var apiKey = "74b0b534002f477ead1026e1b98f1377";
+
+
+
+var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+url += '?' + $.param({
+  'api-key': apiKey,
+  'q': searchTerm
+});
+$.ajax({
+  url: url,
+  method: 'GET',
+}).done(function(result) {
+  console.log(result);
+}).fail(function(err) {
+  throw err;
+});
