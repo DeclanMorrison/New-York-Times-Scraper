c02266a4a217174d8df8ca05e372ec8ce2cb0d8a
Peter Griffin <petergrieve33@gmail.com>
2018-07-18 20:58:32 -0400

    This time I saved my changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,9 @@
 
 
-var searchTerm = "Donald Trump";
+$("#search-btn").on("click", function() {
+
+var searchTerm;
 var apiKey = "74b0b534002f477ead1026e1b98f1377";
-
-
 
 var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
 url += '?' + $.param({
@@ -17,4 +17,9 @@ $.ajax({
   console.log(result);
 }).fail(function(err) {
   throw err;
+});
+
+
+ searchTerm = $("#search-text").text();
+
 });
