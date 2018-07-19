bb0afb7ae724c26d658de960554d41951cbedb26
Eshaffer <edmond.shaffer@gmail.com>
2018-07-18 20:17:05 -0400

    get methof

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,13 @@
 var ApiKey =  "74b0b534002f477ead1026e1b98f1377";
 var querUrl = "http://developer.nytimes.com/article_search_v2.json" + ApiKey;
+
+
+
+
+
+$.ajax({
+    url: querUrl,
+    method:"GET"
+}).then(function(response){
+    
+})
